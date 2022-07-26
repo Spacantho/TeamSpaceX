a8e246cb024d7402bce9aaa2207c455d0c76e582
Spacantho <anthony.tourolle08@gmail.com>
2022-07-26 20:50:37 +0200

    mise au propre, debut template formation

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -2,24 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PagesController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'home')]
     public function home(): Response
     {
         return $this->render('pages/home.html.twig');
     }
 
-    #[Route('/about', name: 'app_about')]
+    #[Route('/about', name: 'about')]
     public function about(): Response
     {
         return $this->render('pages/about.html.twig');
     }
 
+    #[Route('/formation/{formation_id}', name: 'formation', methods: ['GET'])]
+    public function formation(): Response
+    {
+        return $this->render('pages/formation.html.twig');
+    }
    
 
     
